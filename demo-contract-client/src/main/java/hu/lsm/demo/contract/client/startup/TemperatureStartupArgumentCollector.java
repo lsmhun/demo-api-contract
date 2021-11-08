bf7f670d2a6f09611eacfa42921e800e86ce5a56
lsmhun <lsmlogin@gmail.com>
2021-11-08 22:08:39 +0100

    Cloud contract repo structure

--- demo-contract-client/src/main/java/hu/lsm/demo/contract/client/startup/TemperatureStartupArgumentCollector.java
+++ demo-contract-client/src/main/java/hu/lsm/demo/contract/client/startup/TemperatureStartupArgumentCollector.java
@@ -3,7 +3,6 @@ package hu.lsm.demo.contract.client.startup;
 import hu.lsm.demo.contract.client.service.TemperatureService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
